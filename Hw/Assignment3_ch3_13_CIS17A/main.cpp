84ae96a0c6f7d3a6f37603f2ca5998aa38daf273
ta2815530 <taku.arashiro@gmail.com>
2019-06-20 13:40:56 -0700

    Signed-off-by: ta2815530 <taku.arashiro@gmail.com>

---
+++ Hw/Assignment3_ch3_13_CIS17A/main.cpp
@@ -0,0 +1,31 @@
+
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on June 20, 2019, 1:26 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    double totalAge = 10000;
+    
+    cout << setprecision(4) << fixed << showpoint;
+    
+    cout << setw(12) << totalAge;
+    
+    
+    
+    return 0;
+}
+
