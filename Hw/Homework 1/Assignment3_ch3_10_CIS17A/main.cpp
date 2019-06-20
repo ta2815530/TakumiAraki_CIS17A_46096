e27a6044b9538ff4782dedcdbca1b461fcc2fab2
ta2815530 <taku.arashiro@gmail.com>
2019-06-20 14:07:29 -0700

    Signed-off-by: ta2815530 <taku.arashiro@gmail.com>

---
+++ Hw/Homework 1/Assignment3_ch3_10_CIS17A/main.cpp
@@ -0,0 +1,55 @@
+/*
+ * Many financial experts advise that property owners should insure their homes
+ * or buildings for at least 80 % of the amount it would cost to replace the structure.
+ *  Write a program that asks the user to enter the replacement cost of a  building
+ * and then displays the minimum amount of insurance he or she should buy for the property.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on June 20, 2019, 1:50 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    double insurance, replacement;
+    
+    bool good = false;
+    
+    do{
+        cout << "Enter the replacement cost of a building: " << endl;
+        cin >> replacement;
+        
+        if(!replacement || replacement <= 0)
+        {
+            cin.clear();
+            cin.ignore(100000,'\n');
+            cout << "Enter a number greater than 0." << endl;
+        }
+        else
+            good = true;
+    }while(!good);
+
+    
+    insurance = replacement * 0.8;
+    
+    
+    cout << setprecision(2) << fixed << showpoint;
+    
+    cout << "The amount of insurance you may want to prepare is $" << insurance << endl;
+    
+    
+    return 0;
+}
+
