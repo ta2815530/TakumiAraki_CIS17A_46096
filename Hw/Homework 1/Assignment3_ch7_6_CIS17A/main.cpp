6274c8a64043096d74c52d90dcc52d4b0b587947
ta2815530 <taku.arashiro@gmail.com>
2019-06-20 15:17:49 -0700

    Signed-off-by: ta2815530 <taku.arashiro@gmail.com>

---
+++ Hw/Homework 1/Assignment3_ch7_6_CIS17A/main.cpp
@@ -0,0 +1,43 @@
+/*
+ *An amateur meteorologist wants to keep track of weather conditions during the
+ * past year's three-month summer season and has designated each day as either rainy ('R')
+ *  , cloudy ('C'), or sunny ('S'). Write a program that stores this information in a
+ * 3 * 30 array of characters, where the row indicates the month (0=June, 1=July, 2=August)
+ * and the column indicates the day of the month. Note that data are not being collected 
+ * for the 31st of any month. The program should begin by reading the weather data
+ * in from a file. Then it should create a report that displays , for each month 
+ * and for the whole three-month period, how many days were rainy, how many were cloudy, a
+ * nd how any were sunny. It should also report which of the three months and the
+ *  largest number of rainy days. Date for the program can be found in found in
+ * the RainOrShine.txt file.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on June 20, 2019, 2:56 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    ifstream inputFile;
+   
+    
+    
+    
+    
+    
+    return 0;
+}
+
