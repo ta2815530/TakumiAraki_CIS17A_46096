63675dfbe3a1290037c23882396d1db6bbba7e51
Takumi Araki <apndx19@gmail.com>
2019-07-07 21:39:03 -0700

    done with 12.8, might need some modification

---
+++ Hw/Ch12_FIle/Ch12_File_ArrayFileFunction_CIS17A/main.cpp
@@ -0,0 +1,108 @@
+/*
+ //Array/File Functions
+Write a function named arrayToFile. The function should accept three arguments: 
+ * the name of a file, a pointer to an int array, and the size of the array. 
+ * The function should open the specified file in binary mode, write the 
+ * contents of the array to the file, and then close the file.
+Write another function named fileToArray. This function should accept three 
+ * arguments: the name of a file, a pointer to an int array, and the size of the
+ *  array. The function should open the specified file in binary mode, read its
+ *  contents into the array, and then close the file.
+Write a complete program that demonstrates these functions by using the 
+ * arrayToFile function to write an array to a file, and then using the 
+ * fileToArray function to read the data from the same file. //After the data are
+ * read from the file into the array, display the array’s contents on the screen.
+ */
+
+//binary mode -> ios;;binary
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//to write
+void arrayToFile(string,int*,int);      //maybe not [] after *
+//to read
+int *fileToArray(string,int*,int);
+
+int main(int argc, char** argv) {
+    
+    int size=5;
+    string filename="binaryFile";
+    
+    /*
+    cout << "Enter the name of the file that you want to read and write: " << endl;
+    cin >> filename;
+    
+    cout << "Enter the size of the array you want to make: " << endl;
+    cin >> size;
+    */
+    
+    int array[] = {1,2,3,4,5};
+    
+    int *display=nullptr;
+    
+    /*
+    cout << "Enter an integer the array: " << endl;
+    for(int i=0;i<size;i++)
+    {
+        cout << "#" << i+1 << endl;
+        cin >> array[i];
+    }
+    */
+    
+    arrayToFile(filename,array,size);
+    
+    display = fileToArray(filename,array,size);   //what do I need array for? - to read in
+                                        //after reading into an array, I need to
+                                        //display to the screen
+    
+    for(int i=0;i<size;i++)
+    {
+       cout << *(display+i) << " ";
+    }
+    
+    
+    return 0;
+}
+
+void arrayToFile(string name, int *aptr, int size) 
+{
+    ofstream makeFile;
+    
+    makeFile.open(name, ios::binary);
+    
+    if(makeFile)
+    {
+        makeFile.write(reinterpret_cast<char *>(aptr), sizeof(aptr));
+
+        makeFile.close();
+    }
+    else
+    {
+        cout << "Couldn't open the file..." << endl;
+    }
+
+}
+
+int *fileToArray(string name, int *aptr, int size)
+{
+    ifstream readFile;
+    
+    readFile.open(name, ios::binary);
+    
+    if(readFile)
+    {
+        readFile.read(reinterpret_cast<char *>(aptr), sizeof(aptr));
+        
+        readFile.close();
+    }
+    else
+    {
+        cout << "Couldn't open the file..." << endl;
+    }
+
+  
+    return aptr;
+}
