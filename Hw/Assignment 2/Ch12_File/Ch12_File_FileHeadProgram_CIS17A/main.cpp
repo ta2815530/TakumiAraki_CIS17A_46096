70cc8433fbba554f326274ef534345787421da92
Takumi Araki <apndx19@gmail.com>
2019-07-08 21:52:13 -0700

    modification on file head some problem

--- Hw/Assignment 2/Ch12_File/Ch12_File_FileHeadProgram_CIS17A/main.cpp
+++ Hw/Assignment 2/Ch12_File/Ch12_File_FileHeadProgram_CIS17A/main.cpp
@@ -1,16 +1,5 @@
-/*
-//File Head Program
-Write a program that asks the user for the name of a file. The program should 
- * display the first 10 lines of the file on the screen (the “head” of the file). 
- * If the file has fewer
-than 10 lines, the entire file should be displayed, with a message indicating 
- * the entire
-file has been displayed.
- */
-
 #include <iostream>
 #include <fstream>
-#include <ctime>
 
 using namespace std;
 
@@ -24,26 +13,6 @@ int main(int argc, char** argv) {
     
     cout << "Enter the name of the file that you want to read in: " << endl;
     cin >> filename;
-    
-    
-    //////
-    
-    readFile.open(filename, ios::out);
-    
-    srand(0);
-    /*
-    for(int i=0;i<9;i++)
-    {
-        if(i!=8)
-            readFile << rand()%10 +5 << endl;
-        else
-            readFile << "endoffile";
-    }
-    
-    readFile.close();
-    */
-    
-    //////
     
     
     readFile.open(filename, ios::in);
